0bc0d9f5ae96ad9741b43a890d91bfe8afd0a356
Gussarov <vadimgussarov@gmail.com>
2021-03-05 15:36:29 +0300

    Initial commit

---
+++ src/main/java/com/test/test/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.test.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Разработка это круто");
+        return "home";
+    }
+
+}
